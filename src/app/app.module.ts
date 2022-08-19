bbc5e51bb36deef77c2a8d36ab3e6b66fe1d25d4
MinTunA20 <gin.ritani@gmail.com>
2022-08-19 15:17:51 +0700

    product

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PetComponent } from './pet/pet.component';
 import {FormsModule} from "@angular/forms";
+import { ProductComponent } from './product/product.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PetComponent
+    PetComponent,
+    ProductComponent
   ],
     imports: [
         BrowserModule,
